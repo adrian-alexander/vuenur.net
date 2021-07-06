47c8a587973c6d7a7f31cce3c4e20bd355ebd082
Adrian <adrianalexander17@gmail.com>
2021-07-06 17:24:11 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# hello-world
+# app
 
 ## Project setup
 ```
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,28 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div
+    id="top"
+    class="w-full text-lunur-100 from-black via-black to-lunur-700 bg-gradient-to-b relative">
+    <nav id="navbar" class="sticky top-0 z-10 bg-black">
+      <div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+        <div class="relative flex items-center justify-between h-16">
+          <HamburgerButton />
+          <LogoAndText />
+          <Links />
+          <Works />
+        </div>
+      </div>
+    </nav>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import HamburgerButton from "../components/HamburgerButton.vue";
+import LogoAndText from "../components/LogoAndText.vue";
+import Links from "../components/Links.vue";
+import Works from "../components/Works.vue";
 
 export default {
   name: "Home",
-  components: {
-    HelloWorld,
-  },
+  components: { HamburgerButton, LogoAndText, Links, Works },
 };
 </script>
